097e9bc10edf6761ddbddb3d59812ebeb50be460
kaionwong <kaion@ualberta.ca>
2024-11-05 12:52:40 -0700

    finish first pass of field matching between the same table from Analytics and Oracle db

--- on_demand_db_query.sql
+++ on_demand_db_query.sql
@@ -4,13 +4,16 @@ ORDER BY id ASC
 
 
 
-
+--- Get all column name and field type for a table
+SELECT column_name, data_type
+FROM information_schema.columns
+WHERE table_name = 'oracle_cl_objects'
+order by column_name
 
 
 
 
 --- Warning: Deletion
----
-	
+--- This command deletes the public schema and all objects (tables, views, functions, etc.) contained within it. Then it recreates the public schema as an empty schema.
 DROP SCHEMA public CASCADE;
 CREATE SCHEMA public;
